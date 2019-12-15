15c1cb660000765fc4e36d4b4ba6d3e88923de15
kelly r maass <kmaass77627@gmail.com>
2019-12-15 03:37:06 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -12,7 +12,8 @@ def words_starting_with_un_and_ending_with_ing(text)
 end
 
 def words_five_letters_long(text)
-  text.scan()
+  return_array = text.split(" ")
+  return_array.grep(/\b\w{5}\b/)
 
 end
 
