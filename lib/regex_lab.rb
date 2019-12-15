85b04aaea50f3b8ae35300399ab89ef7401b4a44
kelly r maass <kmaass77627@gmail.com>
2019-12-15 03:11:27 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,5 +1,8 @@
 def starts_with_a_vowel?(word)
-if word.match(/[aeiou]/)
+if word.match(/^[AEIOU]|^[aeiou]/)
+    true
+  else
+    false
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
