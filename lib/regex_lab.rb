abcb34a89e67cd7ebec099f73f1eb558de647a6f
kelly_maass <kmaass77627@gmail.com>
2023-02-11 11:13:52 -0600

    new

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,36 +1,64 @@
 def starts_with_a_vowel?(word)
-  if word.match(/^[AEIOU]|^[aeiou]/)
+  if word.match(/^[aeiouAEIOU]/)
     true
   else
     false
-  end
-end 
-
-
-def words_starting_with_un_and_ending_with_ing(text)
- text.scan(/un\w+ing/)
+end
 end
 
-def words_five_letters_long(text)
-  return_array = text.split(" ")
-  return_array.grep(/\b\w{5}\b/)
-
+def words_starting_with_un_and_ending_with_ing(word)
+  word.scan(/\bun\w*ing\b/)
 end
 
-def first_word_capitalized_and_ends_with_punctuation?(text)
-  first_letter = text.scan(/\w+/)[0]
-  if (text.scan(/\./)) && (first_letter == first_letter.upcase)
+def words_five_letters_long(word)
+  word.scan(/\b\w{5}\b/)
+end
+
+def first_word_capitalized_and_ends_with_punctuation?(word)
+  if word.match(/^[A-Z][\s\S]*?[.!?](?:\n\n|\z)/)
     true
-  else  
+  else
     false
-  end
+end
 end
 
-
-def valid_phone_number?(phone)
-  if phone.match(/([0-9] ?){10}/) || phone.match(/(\([0-9]{3}\)([0-9]{3}-[0-9]{4})\b)/) || phone.match(/\b([0-9]{7})\b/)
+def valid_phone_number?(word)
+  if word.match(/\d{3}-\d{3}-\d{4}/)
     true
   else
     false
   end
 end
+
+
+
+
+
+  #def starts_with_a_vowel?(word)
+ # if word.match(/^[aeiouAEIOU]/)
+  #  true
+ # else
+  #  false
+  #end
+  #end 
+
+
+#def words_starting_with_un_and_ending_with_ing(text)
+ # text.scan(/\bun\w+ing\b/)
+#end
+
+#def words_five_letters_long(text)
+#  text.scan(/\w{5}$/)
+#end
+
+#def first_word_capitalized_and_ends_with_punctuation?(text)
+ # if text.scan(//)
+ #   true
+  #else 
+  #  false
+#end
+#end
+
+#def valid_phone_number?(phone)
+  
+#end
